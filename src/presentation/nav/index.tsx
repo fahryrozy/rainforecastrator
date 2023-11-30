ed2b47515c2d338dc5465f96db8097555a38a795
fahryrozy <fahryrozysrg@gmail.com>
2023-11-30 20:29:15 +0700

    setup name and icon

--- src/presentation/nav/index.tsx
+++ src/presentation/nav/index.tsx
@@ -1,6 +1,8 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import BootSplash from 'react-native-bootsplash';
+import {AnimatedBootSplash} from '@presentation/screens/Splash/SplashScreen';
 
 import NetInfo from '@react-native-community/netinfo';
 
@@ -23,6 +25,7 @@ import Home from '@presentation/screens/Home/Home';
 import Search from '@presentation/screens/Search/Search';
 
 const Router = () => {
+  const [visible, setVisible] = useState(true);
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener(state => {
       console.log(state);
@@ -34,9 +37,22 @@ const Router = () => {
       unsubscribe();
     };
   }, []);
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
 
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      onReady={() => {
+        BootSplash.hide();
+      }}>
       <Stack.Navigator initialRouteName="Home" screenOptions={CustomTransition}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Search" component={Search} />
